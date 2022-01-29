8a559e007fd8640a048732bcf42a056a7bf9e11b
nvhnam <danielnam2001@gmail.com>
2022-01-30 03:29:16 +0700

    Embeded FacebookPost 1

--- components/Home/HomeElement.js
+++ components/Home/HomeElement.js
@@ -27,6 +27,7 @@ export const HomeRow2 = styled.div`
   max-width: 1200px;
   display: flex;
   align-content: center;
+  flex-direction: row;
   justify-content: space-between;
   padding: 3rem 1.8rem;
   margin: 5rem 0;
@@ -35,6 +36,7 @@ export const HomeRow2 = styled.div`
   @media screen and (max-width: 767px) {
     flex-direction: column;
     padding: 2rem 1.5rem;
+    justify-content: center;
     margin: 2rem 0;
   }
 `;
@@ -42,19 +44,21 @@ export const HomeRow2 = styled.div`
 export const HomeCol1 = styled.div`
   display: flex;
   align-content: center;
-  justify-self: left;
-  width: 50%;
-  margin-left: auto;
+  width: 45%;
+  margin-left: 4rem;
   margin-right: auto;
+  flex-direction: column;
 
   @media screen and (max-width: 1024px) {
     width: 40%;
   }
 
   @media screen and (max-width: 767px) {
-    width: 85%;
+    width: 90%;
     justify-self: center;
     margin-bottom: 1rem;
+    margin-top: 2rem;
+    margin-left: auto;
   }
 `;
 
@@ -64,51 +68,31 @@ export const HomeCol1Wrapper = styled.div`
   display: block;
 `;
 
-export const HomeCol2 = styled.div`
-  display: flex;
-  align-content: center;
-  justify-self: right;
-  width: 48%;
-  margin-right: -3rem;
-  margin-left: auto;
-
-  @media screen and (max-width: 1024px) {
-    width: 50%;
-    margin-right: auto;
-  }
-
-  @media screen and (max-width: 767px) {
-    width: 85%;
-    justify-self: center;
-    margin-left: auto;
-    margin-right: auto;
-  }
-`;
-
-export const HomeCol2Wrapper = styled.div`
+export const HomeCol12Wrapper = styled.div`
   width: 100%;
-  height: 100%;
+  height: auto;
   padding: 2rem;
   background-color: rgba(0, 0, 0, 0.63);
   border-radius: 15px;
+  margin-top: 1.7rem;
 
   @media screen and (max-width: 767px) {
     padding: 1rem;
   }
 `;
 
-export const HomeCol2Text1 = styled.h1`
+export const HomeCol12Text1 = styled.h1`
   font-size: 1rem;
   color: white;
   font-style: italic;
 `;
 
-export const HomeCol2Text2 = styled.h1`
+export const HomeCol12Text2 = styled.h1`
   font-size: 1.3rem;
   color: white;
 `;
 
-export const HomeCol2Para = styled.p`
+export const HomeCol12Para = styled.p`
   font-size: 0.7rem;
   color: white;
   margin-bottom: 2rem;
@@ -118,13 +102,91 @@ export const HomeCol2Para = styled.p`
   }
 `;
 
-export const HomeCol2Button = styled.div`
+export const HomeCol12Button = styled.div`
   display: flex;
   align-content: center;
+  justify-content: center;
 
   @media screen and (max-width: 767px) {
     justify-content: center;
     margin-left: auto;
     margin-right: auto;
+  }
+`;
+
+export const HomeCol2 = styled.div`
+  display: flex;
+  align-content: center;
+  width: 50%;
+  height: 50%;
+  margin-right: auto;
+  margin-left: auto;
+  padding-left: 6rem;
+  flex-direction: column;
+
+  @media screen and (max-width: 1024px) {
+    width: 50%;
+    padding-left: 0;
+    margin-right: auto;
+  }
+
+  @media screen and (max-width: 767px) {
+    width: 100%;
+    justify-self: center;
+  }
+`;
+
+export const HomeCol2Wrapper = styled.div`
+  width: 100%;
+  height: auto;
+  padding: 2rem;
+  margin-top: -3.5rem;
+
+  @media screen and (max-width: 767px) {
+    padding: 1rem;
+    width: auto;
+  }
+`;
+
+export const HomeCol2Text1 = styled.h1`
+  font-size: 3rem;
+  color: #f8fffc;
+  margin-top: 0.5rem;
+
+  @media screen and (max-width: 767px) {
+    font-size: 2rem;
+    margin-top: 8rem;
+    margin-bottom: 1.9rem;
+  }
+`;
+
+export const HomeCol2Underline = styled.div`
+  display: block;
+  width: 70%;
+  margin-top: -1.5rem;
+  border-bottom: 4px solid #772f1a;
+`;
+
+export const FacebookPostWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+  align-items: center;
+  justify-content: center;
+  background-color: #f8fffc;
+  width: auto;
+  height: auto;
+  margin-top: 2rem;
+  border-radius: 4px;
+
+  @media screen and (max-width: 767px) {
+    /* width: 10rem; */
+    margin-left: -1.8rem;
+    margin-right: auto;
+    margin-bottom: 3rem;
+    /* padding: 5rem; */
+  }
+
+  @media screen and (max-width: 414px) {
+    margin-left: -0.5rem;
   }
 `;
--- pages/index.js
+++ pages/index.js
@@ -10,21 +10,27 @@ import {
   HomeCol1,
   HomeCol2,
   HomeCol1Wrapper,
+  HomeCol12Wrapper,
+  HomeCol12Text1,
+  HomeCol12Text2,
+  HomeCol12Para,
+  HomeCol12Button,
   HomeCol2Wrapper,
   HomeCol2Text1,
-  HomeCol2Text2,
-  HomeCol2Para,
-  HomeCol2Button,
+  HomeCol2Underline,
+  FacebookPostWrapper,
+  FacebookPostWrapper1,
 } from "../components/Home/HomeElement";
 import Home1 from "../components/Home/Home1";
-
+import React from "react";
+import { FacebookProvider, EmbeddedPost } from "react-facebook";
 export default function Home() {
   return (
     <>
       <Head>
         <title>Joys Bakery</title>
         <meta name="description" content="Joys coffee and bakery homepage" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="Joys icon" href="./img/LogoTrans1.png" />
       </Head>
       <HomeContainer>
         <HomeWrapper>
@@ -38,30 +44,43 @@ export default function Home() {
                   src={about1}
                   alt="Joys coffee and bakery menu picture"
                 />
+                <HomeCol12Wrapper>
+                  <HomeCol12Text1>About</HomeCol12Text1>
+                  <HomeCol12Text2>Coffee and Cake</HomeCol12Text2>
+                  <HomeCol12Para>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Illum animi velit amet maiores iste numquam corporis quia
+                    dolore. Minus, iusto?
+                  </HomeCol12Para>
+                  <HomeCol12Button>
+                    <Link href="/about" passHref>
+                      <Button
+                        isBig={false}
+                        isBlack={true}
+                        isWhite={true}
+                        bigFont={false}
+                        withBorder={false}
+                      >
+                        Read more
+                      </Button>
+                    </Link>
+                  </HomeCol12Button>
+                </HomeCol12Wrapper>
               </HomeCol1Wrapper>
             </HomeCol1>
             <HomeCol2>
               <HomeCol2Wrapper>
-                <HomeCol2Text1>Menu</HomeCol2Text1>
-                <HomeCol2Text2>Coffee and Bakery</HomeCol2Text2>
-                <HomeCol2Para>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-                  animi velit amet maiores iste numquam corporis quia dolore.
-                  Minus, iusto?
-                </HomeCol2Para>
-                <HomeCol2Button>
-                  <Link href="/menu" passHref>
-                    <Button
-                      isBig={false}
-                      isBlack={true}
-                      isWhite={true}
-                      bigFont={false}
-                      withBorder={false}
-                    >
-                      Read more
-                    </Button>
-                  </Link>
-                </HomeCol2Button>
+                <HomeCol2Text1>Lastest Post</HomeCol2Text1>
+                <HomeCol2Underline />
+                <FacebookPostWrapper>
+                  <FacebookProvider appId="3032674603645034">
+                    <EmbeddedPost
+                      href="https://www.facebook.com/JoySBakery2022/posts/1061489511373564"
+                      width="200px"
+                      height="100"
+                    />
+                  </FacebookProvider>
+                </FacebookPostWrapper>
               </HomeCol2Wrapper>
             </HomeCol2>
           </HomeRow2>
